816c481171fb27f9665261230381f1476488d7d2
karczi <k.kozlinka@zsebydgoszcz.onmicrosoft.com>
2023-04-30 18:09:32 +0200

    top sort neighbourhood matrix

--- ex3/dag.py
+++ ex3/dag.py
@@ -1,5 +1,6 @@
 from random import randint, choices
 from time import sleep
+from copy import deepcopy
 
 class DAG:
 
@@ -11,6 +12,20 @@ class DAG:
         self.vertices = int(vertices)
         self.saturation = saturation
         self.matrix = []
+        
+        # matrix used for top sort testing (correct top sort is 1, 10, 5, 2, 3, 7, 8, 4, 6, 9)
+        # self.matrix = [
+        #     [0,1,0,0,0,0,0,0,0,1],
+        #     [-1,0,1,0,-1,0,1,0,0,0],
+        #     [0,-1,0,1,0,0,0,0,0,0],
+        #     [0,0,-1,0,0,1,0,-1,0,0],
+        #     [0,1,0,0,0,0,1,0,0,-1],
+        #     [0,0,0,-1,0,0,-1,-1,1,-1],
+        #     [0,-1,0,0,-1,1,0,1,1,0],
+        #     [0,0,0,1,0,1,-1,0,1,0],
+        #     [0,0,0,0,0,-1,-1,-1,0,0],
+        #     [-1,0,0,0,1,1,0,0,0,0]
+        # ]
         self.possible_edges = []
     
     def create_neighbourhood_matrix(self) -> list:
@@ -26,15 +41,36 @@ class DAG:
             self.matrix[item[0]][item[1]] = 1
             self.matrix[item[1]][item[0]] = -1
 
-        print(*[f"{i + 1}," for i in range(self.vertices)])
-        for i in range(self.vertices):
-            print(self.matrix[i]) 
+        # print(*[f"{i + 1}," for i in range(self.vertices)])
+        # for i in range(self.vertices):
+        #     print(self.matrix[i]) 
 
         return self.matrix
     
-    def top_sort(self, elem: tuple):
-        
-        pass
+    #Kahn algorithm
+    def top_sort_neighbourhood_matrix(self):
+        # first element will always be "1" vertex
+        top_sort_list = []
+        lookup_matrix = deepcopy(self.matrix)
+        # independent_vertex = lookup_matrix[0]
+        # index = self.matrix.index(independent_vertex)
+        # top_sort_list.append(index + 1) # we count from 1, not 0
+        #     # for elem in vertex:
+        for _ in lookup_matrix:
+            for index, vertex in enumerate(lookup_matrix):
+                candidate = True
+                for elem in vertex:
+                    if elem == -1:
+                        candidate = False
+                        break
+                if candidate and (index + 1) not in top_sort_list:
+                    top_sort_list.append(index + 1)
+                    for vertex in lookup_matrix:
+                        vertex[index] = 0
+                    break
+        print(top_sort_list)
+            
+
 
 
     def get_vertices(self):
@@ -48,3 +84,4 @@ class DAG:
     
 dag = DAG(vertices=10, saturation=0.35)
 dag.create_neighbourhood_matrix()
+dag.top_sort_neighbourhood_matrix()
