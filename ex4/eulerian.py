f066488e8df5a7289d3cb0f7655d925dd75c0223
Piotr <piotr.las@protonmail.com>
2023-05-18 22:52:00 +0200

    add measuring performance

--- ex4/eulerian.py
+++ ex4/eulerian.py
@@ -19,6 +19,7 @@ class Graph:
         self.generate_adjacency_matrix()
         self.adjacency_matrix_copy = deepcopy(self.adjacency_matrix)
         self.not_found = True
+        self.sum_edges = 0
 
     # generate adjacency matrix for connected graph that is Eulerian and Hamiltonian
     def generate_adjacency_matrix(self):
@@ -29,24 +30,25 @@ class Graph:
             self.adjacency_matrix[nodes[i]][nodes[(i + 1) % self.num_nodes]] = 1
             self.adjacency_matrix[nodes[(i + 1) % self.num_nodes]][nodes[i]] = 1
 
+        self.sum_edges = self.num_nodes
         # add random triangles
-        while sum([sum(node) for node in self.adjacency_matrix]) / 2 < self.num_edges:
+        while self.sum_edges < self.num_edges:
             # choose three random nodes
             three_nodes = random.sample(range(self.num_nodes), 3)
 
             # if there is no edge any of the two nodes, add edge between them
             if self.adjacency_matrix[three_nodes[0]][three_nodes[1]] == 0 and \
-                self.adjacency_matrix[three_nodes[0]][three_nodes[2]] == 0 and \
-                self.adjacency_matrix[three_nodes[1]][three_nodes[2]] == 0:
+                    self.adjacency_matrix[three_nodes[0]][three_nodes[2]] == 0 and \
+                    self.adjacency_matrix[three_nodes[1]][three_nodes[2]] == 0:
                 self.adjacency_matrix[three_nodes[0]][three_nodes[1]] = 1
                 self.adjacency_matrix[three_nodes[1]][three_nodes[0]] = 1
                 self.adjacency_matrix[three_nodes[0]][three_nodes[2]] = 1
                 self.adjacency_matrix[three_nodes[2]][three_nodes[0]] = 1
                 self.adjacency_matrix[three_nodes[1]][three_nodes[2]] = 1
                 self.adjacency_matrix[three_nodes[2]][three_nodes[1]] = 1
+                self.sum_edges += 3
 
-        # update number of edges
-        self.num_edges = sum([sum(node) for node in self.adjacency_matrix]) // 2
+        self.num_edges = self.sum_edges
 
     def find_hamilton_cycle(self, vertex=0):
         self.hamilton_cycle.append(vertex)
@@ -87,32 +89,158 @@ class Graph:
                 circuit.append(node)
                 location = stack[-1] if stack else 0
 
+    def isSafe(self, v, graph, path, pos):
+
+        # If the vertex is adjacent to
+        # the vertex of the previously
+        # added vertex
+        if graph[path[pos - 1]][v] == 0:
+            return False
+
+        # If the vertex has already
+        # been included in the path
+        for i in range(pos):
+            if path[i] == v:
+                return False
+
+        # Both the above conditions are
+        # not true, return true
+        return True
+
+    # To check if there exists
+    # at least 1 hamiltonian cycle
+    hasCycle = False
+
+    # Function to find all possible
+    # hamiltonian cycles
+    def hamCycle(self, graph):
+        global hasCycle
+
+        # Initially value of boolean
+        # flag is false
+        hasCycle = False
+
+        # Store the resultant path
+        path = []
+        path.append(0)
+
+        # Keeps the track of the
+        # visited vertices
+        visited = [False] * (len(graph))
+
+        for i in range(len(visited)):
+            visited[i] = False
+
+        visited[0] = True
+
+        # Function call to find all
+        # hamiltonian cycles
+        self.FindHamCycle(graph, 1, path, visited)
+
+        if hasCycle:
+            # If no Hamiltonian Cycle
+            # is possible for the
+            # given graph
+            print("No Hamiltonian Cycle" + "possible ")
+            return
+
+    # Recursive function to find all
+    # hamiltonian cycles
+    def FindHamCycle(self, graph, pos, path, visited):
+
+        # If all vertices are included
+        # in Hamiltonian Cycle
+        if pos == len(graph):
+
+            # If there is an edge
+            # from the last vertex to
+            # the source vertex
+            if graph[path[-1]][path[0]] != 0:
+
+                # Include source vertex
+                # into the path and
+                # print the path
+                path.append(0)
+                # for i in range(len(path)):
+                #     print(path[i], end=" ")
+                # print()
+
+                # Remove the source
+                # vertex added
+                path.pop()
+
+                # Update the hasCycle
+                # as true
+                hasCycle = True
+            return
+
+        # Try different vertices
+        # as the next vertex
+        for v in range(len(graph)):
+
+            # Check if this vertex can
+            # be added to Cycle
+            if self.isSafe(v, graph, path, pos) and not visited[v]:
+                path.append(v)
+                visited[v] = True
+
+                # Recur to construct
+                # rest of the path
+                self.FindHamCycle(graph, pos + 1, path, visited)
+
+                # Remove current vertex
+                # from path and process
+                # other vertices
+                visited[v] = False
+                path.pop()
     def print_adjacency_matrix(self):
         for row in self.adjacency_matrix:
             print(row)
 
 
-# for i in range(10, 10000, 100):
-#     graph = Graph(0.7, i)
-#     # graph.print_adjacency_matrix()
-#     start = time_ns()
-#     graph.find_hamilton_cycle()
-#     end = time_ns()
-#     print(f"n: {i}, time: {(end - start) / 1000000000} s")
+step = 2
+saturations = [0.5]
+repeats = 10
+
+for j in saturations:
+    average_hamilton_times = []
+    for rep in range(repeats):
+        hamilton_times = []
+        for i in range(3, 18):
+            hamilton_graph = Graph(j, (i + 1) * step)
+
+            start = time_ns()
+            hamilton_graph.hamCycle(hamilton_graph.adjacency_matrix)
+            end = time_ns()
+            print(f"n: {i}, time: {(end - start) / 1000000000} s")
+            hamilton_times.append([(i + 1) * step, end - start])
+
+        average_hamilton_times.append(hamilton_times)
+
+    for test in range(len(average_hamilton_times[0])):
+        sum = 0
+        for rep in range(repeats):
+            sum += average_hamilton_times[rep][test][1]
+        average_hamilton_times[0][test][1] = sum / repeats
+
+    with open(f"euler-hamilton_{j}.txt", "w") as file:
+        for line in average_hamilton_times[0]:
+            file.write(f"{line}\n")
 #
-graph = Graph(0.7, 6)
-graph.print_adjacency_matrix()
-graph.find_hamilton_cycle()
-adj_matrix = graph.adjacency_matrix
-G = nx.Graph()
-for i in range(len(adj_matrix)):
-    for j in range(i + 1, len(adj_matrix[i])):
-        if adj_matrix[i][j] == 1:
-            G.add_edge(i, j)
-# Draw the graph
-nx.draw(G, with_labels=True, node_color='lightblue', node_size=500, font_weight='bold')
-# Display the graph
-plt.show()
+# graph = Graph(0.5, 10)
+# # graph.print_adjacency_matrix()
+# # graph.find_hamilton_cycle()
+# graph.hamCycle(graph.adjacency_matrix)
+# adj_matrix = graph.adjacency_matrix
+# G = nx.Graph()
+# for i in range(len(adj_matrix)):
+#     for j in range(i + 1, len(adj_matrix[i])):
+#         if adj_matrix[i][j] == 1:
+#             G.add_edge(i, j)
+# # Draw the graph
+# nx.draw(G, with_labels=True, node_color='lightblue', node_size=500, font_weight='bold')
+# # Display the graph
+# plt.show()
 
 
 # graph.find_eulerian_cycle()
--- ex4/hamiltonian_eulerian.py
+++ ex4/hamiltonian_eulerian.py
@@ -1,5 +1,5 @@
 import random
-from random import shuffle, choice, randint
+from random import shuffle
 from time import time_ns
 from copy import deepcopy
 import networkx as nx
@@ -12,13 +12,12 @@ class Graph:
         self.num_nodes = num_nodes
         self.num_edges = int(self.num_nodes * (self.num_nodes - 1) / 2 * self.saturation)
         self.adjacency_matrix = [[0 for _ in range(self.num_nodes)] for _ in range(self.num_nodes)]
-        self.odd_nodes = []
         self.hamilton_cycle = []
         self.euler_cycle = []
-        self.num_edges_graph = self.num_edges
         self.generate_adjacency_matrix()
         self.adjacency_matrix_copy = deepcopy(self.adjacency_matrix)
         self.not_found = True
+        self.sum_edges = 0
 
     # generate adjacency matrix for connected graph that is Eulerian and Hamiltonian
     def generate_adjacency_matrix(self):
@@ -29,161 +28,152 @@ class Graph:
             self.adjacency_matrix[nodes[i]][nodes[(i + 1) % self.num_nodes]] = 1
             self.adjacency_matrix[nodes[(i + 1) % self.num_nodes]][nodes[i]] = 1
 
+        self.sum_edges = self.num_nodes
         # add random triangles
-        first_node = nodes[0]
-        while sum([sum(node) for node in self.adjacency_matrix]) / 2 < self.num_edges:
-            while True:
-                connections = [i for i, x in enumerate(self.adjacency_matrix[first_node]) if x == 0]
-                connections.remove(first_node)
-                if len(connections) == 0:
-                    first_node = random.randint(0, self.num_nodes - 1)
-                    continue
+        while self.sum_edges < self.num_edges:
+            # choose three random nodes
+            three_nodes = random.sample(range(self.num_nodes), 3)
 
-                for conn in connections:
-                    second_node = choice(connections)
-                    connections = [i for i, x in enumerate(self.adjacency_matrix[second_node]) if x == 0]
-                    connections.remove(first_node)
-                    connections.remove(second_node)
-                    if len(connections) == 0:
-                        second_node = choice(connections)
-                        connections = [i for i, x in enumerate(self.adjacency_matrix[second_node]) if x == 0]
-                        connections.remove(first_node)
-                        connections.remove(second_node)
-                    third_node = choice(connections)
-                    if self.adjacency_matrix[first_node][third_node] == 0:
-                        self.adjacency_matrix[first_node][third_node] = 1
-                        self.adjacency_matrix[third_node][first_node] = 1
-                        self.adjacency_matrix[second_node][third_node] = 1
-                        self.adjacency_matrix[third_node][second_node] = 1
-                        self.adjacency_matrix[first_node][second_node] = 1
-                        self.adjacency_matrix[second_node][first_node] = 1
-                        break
+            # if there is no edge any of the two nodes, add edge between them
+            if self.adjacency_matrix[three_nodes[0]][three_nodes[1]] == 0 and \
+                self.adjacency_matrix[three_nodes[0]][three_nodes[2]] == 0 and\
+                self.adjacency_matrix[three_nodes[1]][three_nodes[2]] == 0:
+                self.adjacency_matrix[three_nodes[0]][three_nodes[1]] = 1
+                self.adjacency_matrix[three_nodes[1]][three_nodes[0]] = 1
+                self.adjacency_matrix[three_nodes[0]][three_nodes[2]] = 1
+                self.adjacency_matrix[three_nodes[2]][three_nodes[0]] = 1
+                self.adjacency_matrix[three_nodes[1]][three_nodes[2]] = 1
+                self.adjacency_matrix[three_nodes[2]][three_nodes[1]] = 1
+                self.sum_edges += 3
 
-        # while sum([sum(node) for node in self.adjacency_matrix]) / 2 < self.num_edges:
-        #     connections = [i for i, x in enumerate(self.adjacency_matrix[beg_node])]
-        #     connections.remove(beg_node)
-        #     end_node = choice(connections)
-        #     if sum(self.adjacency_matrix[end_node]) % 2 == 1:
-        #         self.adjacency_matrix[beg_node][end_node] = 1
-        #         self.adjacency_matrix[end_node][beg_node] = 1
-        #         beg_node = choice([i for i, node in enumerate(self.adjacency_matrix) if sum(node) < self.num_nodes - 1])
-        #     else:
-        #         self.adjacency_matrix[beg_node][end_node] = 1
-        #         self.adjacency_matrix[end_node][beg_node] = 1
-        #         beg_node = end_node
-        #
-        # for i, node in enumerate(self.adjacency_matrix):
-        #     if sum(node) % 2 == 1:
-        #         for j in range(i+1, len(node)):
-        #             if sum(self.adjacency_matrix[j]) % 2 == 1:
-        #                 if node[j] == 0:
-        #                     self.adjacency_matrix[i][j] = 1
-        #                     self.adjacency_matrix[j][i] = 1
-        #                     break
-        #                 else:
-        #                     self.adjacency_matrix[i][j] = 0
-        #                     self.adjacency_matrix[j][i] = 0
-        #                     break
+        self.num_edges = self.sum_edges
 
-        self.num_edges = sum([sum(node) for node in self.adjacency_matrix]) // 2
-
-    def find_Hamilton_cycle(self, vertex=0):
+    def find_hamilton_cycle(self, vertex=0):
         self.hamilton_cycle.append(vertex)
-        print(self.hamilton_cycle)
-        # może się pojawiac jeden wierzchołek wiele razy
         possible_neighbours = list(set([i for i, x in enumerate(self.adjacency_matrix[vertex]) if x == 1]).difference(self.hamilton_cycle))
         for neighbour in possible_neighbours:
             # return only one Hamilton cycle
             if self.not_found:
-                self.find_Hamilton_cycle(neighbour)
+                self.find_hamilton_cycle(neighbour)
 
         if len(self.hamilton_cycle) == self.num_nodes and self.hamilton_cycle[0] in [x for x, i in enumerate(self.adjacency_matrix[self.hamilton_cycle[-1]]) if i == 1] and self.not_found:
-            print("cykl znaleziony")
-            for node in self.hamilton_cycle:
-                print(f"{node}", end=" -> ")
-            print(f"{self.hamilton_cycle}")
+            # print("cykl znaleziony")
+            # for node in self.hamilton_cycle:
+            #     print(f"{node}", end=" -> ")
+            # print(f"{self.hamilton_cycle}")
             self.not_found = False
-            # self.hamilton_cycle.remove(vertex)
-            # return
         else:
             self.hamilton_cycle.remove(vertex)
-            # return
 
     def find_eulerian_cycle(self):
         location = 0
         stack = [location]
-        circuit = []
-        # adj_matrix = self.adjacency_matrix
-        # G = nx.Graph()
-        # for i in range(len(adj_matrix)):
-        #     for j in range(i + 1, len(adj_matrix[i])):
-        #         if adj_matrix[i][j] == 1:
-        #             G.add_edge(i, j)
-        # # Draw the graph
-        # nx.draw(G, with_labels=True, node_color='lightblue', node_size=500, font_weight='bold')
-        # # Display the graph
-        # plt.show()
-        # print(self.num_edges)
-        while len(circuit) < self.num_edges + 1:
+        while len(self.euler_cycle) < self.num_edges + 1:
             for i, vertex in enumerate(self.adjacency_matrix_copy[location]):
                 if vertex == 1:
-                    # print(f"location: {location}, i: {i}, stack: {stack}, circuit: {circuit}")
                     self.adjacency_matrix_copy[location][i] = 0
                     self.adjacency_matrix_copy[i][location] = 0
-                    # G.remove_edge(location, i)
-                    # nx.draw(G, with_labels=True, node_color='lightblue', node_size=500, font_weight='bold')
-                    # plt.show()
                     stack.append(i)
                     location = i
                     break
             else:
                 node = stack.pop()
-                circuit.append(node)
-                # print(f"location: {location}, stack: {stack}, circuit: {circuit}")
+                self.euler_cycle.append(node)
                 location = stack[-1] if stack else 0
+
 
     def print_adjacency_matrix(self):
         for row in self.adjacency_matrix:
             print(row)
 
+step = 2
+saturations = [0.3, 0.7]
+repeats = 10
 
-# for i in range(10, 10000, 100):
-#     graph = Graph(0.7, i)
-#     # graph.print_adjacency_matrix()
-#     start = time_ns()
-#     graph.find_Hamilton_cycle()
-#     end = time_ns()
-#     print(f"n: {i}, time: {(end - start) / 1000000000} s")
+
+for j in saturations:
+    # average_euler_times = []
+    # for rep in range(repeats):
+    #     euler_times = []
+    #     for i in range(15):
+    #         euler_graph = Graph(j, (i+1)*step)
+    #
+    #         start = time_ns()
+    #         euler_graph.find_eulerian_cycle()
+    #         end = time_ns()
+    #         print(f"n: {i}, time: {(end - start) / 1000000000} s")
+    #         euler_times.append([(i+1)*step, end - start])
+    #
+    #     average_euler_times.append(euler_times)
+    #
+    # for test in range(len(average_euler_times[0])):
+    #     sum = 0
+    #     for rep in range(repeats):
+    #         sum += average_euler_times[rep][test][1]
+    #     average_euler_times[0][test][1] = sum / repeats
+
+    # with open(f"euler_{j}.txt", "w") as file:
+    #     for line in average_euler_times[0]:
+    #         file.write(f"{line}\n")
+
+    average_hamilton_times = []
+    for rep in range(repeats):
+        hamilton_times = []
+        for i in range(3, 18):
+            hamilton_graph = Graph(j, (i + 1) * step)
+
+            start = time_ns()
+            hamilton_graph.find_hamilton_cycle()
+            end = time_ns()
+            print(f"n: {i}, time: {(end - start) / 1000000000} s")
+            hamilton_times.append([(i + 1) * step, end - start])
+
+        average_hamilton_times.append(hamilton_times)
+
+    for test in range(len(average_hamilton_times[0])):
+        sum = 0
+        for rep in range(repeats):
+            sum += average_hamilton_times[rep][test][1]
+        average_hamilton_times[0][test][1] = sum / repeats
+
+    with open(f"hamilton_{j}.txt", "w") as file:
+        for line in average_hamilton_times[0]:
+            file.write(f"{line}\n")
+
+
+# graph = Graph(0.3, 30)
+# graph.print_adjacency_matrix()
+# graph.find_eulerian_cycle()
+# print(graph.circuit, len(graph.circuit), graph.num_edges)
+# graph.find_hamilton_cycle()
 #
-graph = Graph(0.7, 6)
-graph.print_adjacency_matrix()
-
-adj_matrix = graph.adjacency_matrix
-G = nx.Graph()
-for i in range(len(adj_matrix)):
-    for j in range(i + 1, len(adj_matrix[i])):
-        if adj_matrix[i][j] == 1:
-            G.add_edge(i, j)
-# Draw the graph
-nx.draw(G, with_labels=True, node_color='lightblue', node_size=500, font_weight='bold')
-# Display the graph
-plt.show()
+#
+# adj_matrix = graph.adjacency_matrix
+# G = nx.Graph()
+# for i in range(len(adj_matrix)):
+#     for j in range(i + 1, len(adj_matrix[i])):
+#         if adj_matrix[i][j] == 1:
+#             G.add_edge(i, j)
+# # Draw the graph
+# nx.draw(G, with_labels=True, node_color='lightblue', node_size=500, font_weight='bold')
+# # Display the graph
+# plt.show()
 
 
 # graph.find_eulerian_cycle()
-# graph.find_Hamilton_cycle()
+# graph.find_hamilton_cycle()
 
 # count = 0
 # for i in range(1000):
 #     i_count = 0
-#     graph = Graph(0.7, 25)
+#     graph = Graph(0.7, (i+1)*7)
 #     print(graph.num_edges)
-#     graph.find_eulerian_cycle()
+#     graph.find_hamilton_cycle()
 #     # print(graph.find_eulerian_cycle_wrapper(deepcopy(graph.adjacency_matrix)))
 #     # print(graph.find_eulerian_cycle())
 #     for row in graph.adjacency_matrix:
 #         if sum(row) % 2 == 1:
-#             # print("nieeulerowski")
+#             print("nieeulerowski")
 #             i_count += 1
 #             count += 1
 #
